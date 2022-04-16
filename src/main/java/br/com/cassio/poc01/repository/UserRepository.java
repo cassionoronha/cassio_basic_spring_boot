cccce9d950119543adeb18e439f5fafe8e413331
Cassio Noronha <cassionoronha@gmail.com>
2022-04-16 20:32:49 -0300

    postgres added to project

--- src/main/java/br/com/cassio/poc01/repository/UserRepository.java
+++ src/main/java/br/com/cassio/poc01/repository/UserRepository.java
@@ -1,13 +1,13 @@
 package br.com.cassio.poc01.repository;
 
 import br.com.cassio.poc01.model.UserSistema;
-import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 import java.util.List;
 
 @RepositoryRestResource(path = "usuario", collectionResourceRel = "usuarios")
-public interface UserRepository extends MongoRepository<UserSistema, String> {
-    List<UserSistema> findByUsername(@Param("name") String name);
+public interface UserRepository extends PagingAndSortingRepository<UserSistema, Long> {
+    List<UserSistema> findByUsername(String name);
 }
