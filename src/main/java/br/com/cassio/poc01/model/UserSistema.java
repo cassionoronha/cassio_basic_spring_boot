d4c5af3f2b54b746764d54ddbe0b746c9a378497
cassionoronha <cassionoronha@gmail.com>
2022-04-16 20:44:45 -0300

    Feature/postgres (#3)
    * Spring Actuator included
    * postgres added to project

--- src/main/java/br/com/cassio/poc01/model/UserSistema.java
+++ src/main/java/br/com/cassio/poc01/model/UserSistema.java
@@ -4,13 +4,19 @@ package br.com.cassio.poc01.model;
 import lombok.Data;
 import lombok.Getter;
 import lombok.Setter;
-import org.springframework.data.annotation.Id;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
 
 @Data
 @Getter @Setter
+@Entity
 public class UserSistema {
 
     @Id
+    @GeneratedValue
+    private Long id_user;
     private String username;
     private String email;
     private String password;
